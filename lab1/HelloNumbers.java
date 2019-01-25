41de0303fc284cb1e98bd64c1b97c4bef6fb7deb
Austin Nguyen <austinnguyen517@berkeley.edu>
2019-01-24 16:46:10 -0800

    Added semicolons

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -1,9 +1,9 @@
 public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
-        int curr = 0
+        int curr = 0;
         while (x < 10) {
-            curr = curr + x
+            curr = curr + x;
             System.out.print(curr + " ");
             x = x + 1;
         }
