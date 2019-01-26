41adad4a681b5ffcc560bbd250151a30638ee256
Austin Nguyen <austinnguyen517@berkeley.edu>
2019-01-26 03:01:03 -0800

    Edit Body

--- proj0/Body.java
+++ proj0/Body.java
@@ -7,7 +7,7 @@ public class Body {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    public static final double G = 6.67e-11;
+    private static final double G = 6.67e-11;
 
     public Body(double xP, double yP, double xV, double yV, double m, String img){
       /*Constructs new Body*/
